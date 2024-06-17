bba3f2d8d577b14bc2c6309d2a683af412bd5438
Debangshu Nath <thecrusader.25225@gmail.com>
2024-06-17 07:51:14 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <>
       <BrowserRouter>
         <div className="w-screen h-screen">
-          <div className="fixed top-0 left-0  border-b border-black  h-16 w-full shadow-xl"></div>
-          <div className="fixed top-0 left-0 border-black border-r h-full w-16 toggle-visibility shadow-right"></div>
+          <div className="fixed top-0 left-0  border-b border-black  h-24 w-full shadow-xl bg-transparent backdrop-blur z-10"></div>
+          <div className="fixed top-0 left-0 border-black border-r h-full w-24 toggle-visibility shadow-right"></div>
 
           <div className="  w-full h-full ">
             <Routes>
--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,7 @@ export default function Home() {
   const [audioPlayer, setAudioPlayer] = useState("");
   const [currentSong, setCurrentSong] = useState("");
   const [time, setTime] = useState("");
+  const [seeAll, setSeeAll] = useState(false);
 
   const addedSongsRef = useRef(null);
 
@@ -35,13 +36,41 @@ export default function Home() {
     else setTime("Evening");
   }, []);
 
+  function Library() {
+    console.log("See all: " + seeAll);
+    return (
+      <>
+        {addedSongs.map((song) => {
+          return (
+            <div
+              className={` backdrop-blur-3xl w-32 h-32  m-2 ${
+                seeAll ? "flex-grow w-full" : "rounded-full"
+              } hover:scale-125 hover:cursor-pointer transform transition-transform duration-200 ease-linear flex flex-col flex-shrink-0 justify-center items-center shadow-2xl hover:border-y border-purple-600  `}
+              onClick={() =>
+                setCurrentSong({
+                  url: `${song.url}`,
+                  name: `${song.name}`,
+                })
+              }
+            >
+              <p className=" bg-gradient-to-r from-yellow-300 via-pink-500 to-fuchsia-800 bg-clip-text text-transparent ">
+                {song.name}
+              </p>
+              <FaMusic className="text-black" />
+            </div>
+          );
+        })}
+      </>
+    );
+  }
+
   return (
     <>
-      <div className="w-full h-full flex flex-row bg-gradient-to-tl from-slate-900 via-fuchsia-900 to-slate-900 adjustible-padding ">
-        <div className="w-3/4 h-full flex flex-col bg-gradient-to-r from-violet-400 via-fuchsia-100 to-purple-400 bg-clip-text text-transparent  px-8 py-4">
-          <div className="w-full h-1/2 flex flex-col shadow-bottom rounded-3xl px-4">
+      <div className="w-full h-full flex flex-row bg-gradient-to-tl from-slate-900 via-fuchsia-900 to-slate-900 adjustible-padding  overflow-y-auto ">
+        <div className="w-[calc(75%)] h-full flex flex-col bg-gradient-to-r from-violet-400 via-fuchsia-100 to-purple-400 bg-clip-text text-transparent px-8 pt-8 z-0">
+          <div className="w-full flex flex-col shadow-bottom rounded-3xl px-4 h-1/3">
             <div className="flex justify-between items-center  w-full h-full ">
-              <span className=" justify-center pr-0 flex flex-col  italic ">
+              <span className=" justify-center  flex flex-col  italic ">
                 <p className="font-extrabold font-mono adjustable-text-size ">
                   Good {time}, User
                 </p>
@@ -52,7 +81,7 @@ export default function Home() {
               <img
                 src={userLogo}
                 alt="user pfp"
-                className="adjustible-image-size rounded-tr-2xl"
+                className="adjustible-image-size rounded-tr-2xl m-2"
               />
             </div>
             <div className="border-t-2 p-2 flex justify-end">
@@ -67,48 +96,42 @@ export default function Home() {
               />
             </div>
           </div>
-          <div className="p-4 mt-8 mb-4 w-full h-1/4 flex flex-col rounded-b-full">
+          <div className="p-4 mt-8 mb-4 w-full h-auto flex flex-col  flex-grow m-1 ">
             <span className="flex justify-between">
               <p className=" font-bold font-mono">Library</p>
-              {/* <Link to={{ pathname: "/library", state: { addedSongs } }}> */}
-              <button onClick={"#"}>See all</button>
+              <button onClick={() => setSeeAll(!seeAll)}>See all</button>
             </span>
-            <div className=" w-full h-full  flex items-center  overflow-x-auto overflow-y-hidden flex-none rounded-full backdrop-blur-xl shadow-md  ">
-              {addedSongs.map((song) => {
-                return (
-                  <div
-                    className=" backdrop-blur-3xl w-32 h-32 flex-shrink-0 m-2 rounded-full hover:scale-125 hover:cursor-pointer transform transition-transform duration-200 ease-linear flex flex-col justify-center items-center shadow-2xl hover:border-y border-purple-600 "
-                    onClick={() =>
-                      setCurrentSong({
-                        url: `${song.url}`,
-                        name: `${song.name}`,
-                      })
-                    }
-                  >
-                    <p className=" bg-gradient-to-r from-yellow-300 via-pink-500 to-fuchsia-800 bg-clip-text text-transparent ">
-                      {song.name}
-                    </p>
-                    <FaMusic className="text-black" />
-                  </div>
-                );
-              })}
-            </div>
+            {/**library section */}
+            {seeAll ? (
+              <div className=" w-full  flex flex-col flex-grow rounded-3xl backdrop-blur-xl shadow-md flex-wrap flex-shrink-0 m-1 ">
+                <Library />
+              </div>
+            ) : (
+              <div className=" w-full h-full  flex items-center  overflow-x-auto overflow-y-hidden flex-none rounded-full backdrop-blur-xl shadow-md  ">
+                <Library />
+              </div>
+            )}
           </div>
-          <div className=" w-full h-1/4  flex items-center p-4 mb-8 bt-4 overflow-x-auto overflow-y-hidden flex-none rounded-full backdrop-blur-xl shadow-md">
-            {addedSongs.map((song) => {
-              return (
-                <div className=" backdrop-blur-3xl w-32 h-32 m-2 rounded-full hover:scale-150 hover:cursor-pointer transform transition-transform duration-200 ease-linear flex flex-col justify-center items-center shadow-2xl">
-                  {/* <p className=" bg-gradient-to-r from-yellow-300 via-pink-500 to-fuchsia-800 bg-clip-text text-transparent ">
-                    {song.name}
-                  </p> */}
-                  <FaMusic className="text-black w-full" />
-                </div>
-              );
-            })}
+          {/** */}
+          <div className="p-4 mt-8 mb-4 w-full h-auto flex flex-col flex-grow m-1 ">
+            <span className="flex justify-between">
+              <p className=" font-bold font-mono">Playlist</p>
+              <button onClick={() => setSeeAll(!seeAll)}>See all</button>
+            </span>
+            {/**library section */}
+            {seeAll ? (
+              <div className=" w-full  flex items-center  overflow-x-hidden flex-grow rounded-full backdrop-blur-xl shadow-md flex-wrap flex-shrink-0 m-1 ">
+                <Library />
+              </div>
+            ) : (
+              <div className=" w-full h-full  flex items-center  overflow-x-auto overflow-y-hidden flex-none rounded-full backdrop-blur-xl shadow-md  ">
+                <Library />
+              </div>
+            )}
           </div>
         </div>
 
-        <div className="player w-1/3 h-full px-8 py-4 shadow-left">
+        <div className="player w-[calc(25%)] h-full px-8 py-4 shadow-left fixed top-0 right-0">
           <Player song={currentSong} />
           {console.log("Current" + currentSong.url)}
         </div>

ec238a621b900b42d9541a6e16bd4d03ad55b18c
Debangshu Nath <thecrusader.25225@gmail.com>
2024-06-21 19:37:57 +0530

    new style improved

--- src/Home.js
+++ src/Home.js
@@ -50,17 +50,15 @@ export default function Home() {
         {(type == "Library" ? addedSongs : likedSongs).map((song, index) => (
           <div
             key={index}
-            className={`backdrop-blur-3xl m-2 ${
+            className={` m-2 p-2 text-wrap ${
               seeAll[type]
                 ? "flex-grow-0 w-full h-16 border-b border-fuchsia-950 hover:scale-110 hover:bg-white hover:bg-opacity-5 hover:rounded-full duration-100 "
-                : "rounded-full w-32 h-32 hover:border-y border-purple-600 shadow-2xl hover:scale-125 duration-200"
+                : "rounded-full w-32 h-32 hover:border-y border-purple-600 shadow-2xl hover:scale-125 duration-200 bg-inherit "
             } hover:cursor-pointer transform transition-transform ease-linear flex flex-col flex-shrink-0 justify-center items-center`}
             onClick={() => handleSongClick(index, type)}
           >
-            <p className="bg-gradient-to-r from-yellow-300 via-pink-500 to-fuchsia-800 bg-clip-text text-transparent">
-              {song.name}
-            </p>
-            <FaMusic className="text-black" />
+            <p className="text-white">{song.name}</p>
+            {/* <FaMusic className="text-black" /> */}
           </div>
         ))}
       </>
@@ -70,7 +68,7 @@ export default function Home() {
   return (
     <div className="w-full h-full flex flex-row bg-gradient-to-tl from-slate-900 via-fuchsia-900 to-slate-900 adjustible-padding overflow-y-auto ">
       <div className="w-[calc(100%)] h-full flex flex-col custom-text-color px-8 pt-8 mb-24 z-0">
-        <div className="w-full flex flex-col shadow-bottom rounded-3xl px-4 h-1/3 bg-white bg-opacity-10 ">
+        <div className="w-full flex flex-col rounded-3xl px-4 h-1/3 bg-white bg-opacity-10 ">
           <div className="flex justify-between items-center w-full h-full">
             <span className="justify-center flex flex-col italic">
               <p className="font-extrabold font-mono adjustable-text-size">
@@ -104,58 +102,16 @@ export default function Home() {
             <button
               onClick={() => setSeeAll({ ...seeAll, Library: !seeAll.Library })}
             >
-              See all
+              {seeAll.Library ? "Collapse" : "Expand"}
             </button>
           </span>
           {seeAll.Library ? (
-            <div className=" w-full  flex flex-col flex-grow rounded-3xl backdrop-blur-xl shadow-md flex-wrap flex-shrink-0 m-1 p-8 pr-12 ">
+            <div className=" w-full  flex flex-col rounded-3xl backdrop-blur-xl flex-wrap flex-shrink-0 m-1 p-8 pr-12 bg-white bg-opacity-10 ">
               <List type="Library" />
             </div>
           ) : (
-            <div className=" w-full h-full  flex items-center  overflow-x-auto overflow-y-hidden flex-none rounded-full backdrop-blur-xl shadow-md  ">
+            <div className=" w-full h-auto min-h-16 flex items-center  overflow-x-auto overflow-y-hidden flex-none rounded-full bg-white bg-opacity-10  ">
               <List type="Library" />
-            </div>
-          )}
-        </div>
-        <div className="mt-8 mb-4 w-full h-auto flex flex-col flex-grow m-1 text-fuchsia-200">
-          <span className="flex justify-between">
-            <p className="font-bold font-mono">Liked Songs</p>
-            <button
-              className=""
-              onClick={() => setSeeAll({ ...seeAll, Liked: !seeAll.Liked })}
-            >
-              See all
-            </button>
-          </span>
-          {seeAll.Liked ? (
-            <div className=" w-full  flex flex-col flex-grow rounded-3xl backdrop-blur-xl shadow-md flex-wrap flex-shrink-0 m-1 p-8 pr-12">
-              <List type="Liked" />
-            </div>
-          ) : (
-            <div className=" w-full h-full  flex items-center  overflow-x-auto overflow-y-hidden flex-none rounded-full backdrop-blur-xl shadow-md max-h-40  ">
-              <List type="Liked" />
-            </div>
-          )}
-        </div>
-        <div className="mt-8 mb-4 w-full h-auto flex flex-col flex-grow m-1">
-          <span className="flex justify-between">
-            <p className="font-bold font-mono text-fuchsia-200">Playlist</p>
-            <button
-              className="text-fuchsia-200"
-              onClick={() =>
-                setSeeAll({ ...seeAll, Playlist: !seeAll.Playlist })
-              }
-            >
-              See all
-            </button>
-          </span>
-          {seeAll.Playlist ? (
-            <div className=" w-full  flex flex-col flex-grow rounded-3xl backdrop-blur-xl shadow-md flex-wrap flex-shrink-0 m-1 p-8 pr-12">
-              <List type="Playlist" />
-            </div>
-          ) : (
-            <div className=" w-full h-full  flex items-center  overflow-x-auto overflow-y-hidden flex-none rounded-full backdrop-blur-xl shadow-md  ">
-              <List type="Playlist" />
             </div>
           )}
         </div>
